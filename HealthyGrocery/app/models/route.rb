9e71ffef3c512ba8d49bf9ec17c28520f52a3bd5
osherifo <osherifo93@gmail.com>
2014-03-28 00:56:01 +0200

    Documentation omarsherif

--- HealthyGrocery/app/models/route.rb
+++ HealthyGrocery/app/models/route.rb
@@ -1,4 +1,12 @@
+# == Schema Information
+#
+# Table name: routes
+#
+#  id             :integer     not null, primary key
+#  arrOfPoints    :Array
+
 class Route
+
   include Mongoid::Document
   field :arrOfPoints, type: Array
   belongs_to :shipment , counter_cache: true
