361b8e86acfac9299e16fb4703927063544e16f2
karim-elbawab <karim.elbawab@gmail.com>
2014-03-28 00:57:37 +0200

    update my story

---
+++ HealthyGrocery/s/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '3333327ccc6845e871c898cdaa92a3ecd243356777286642c4301e6f62572ba4eb3950d165eb111b09733e2108a20c355329f781d83a5c75ccb6f4bbe80c7fbd';
