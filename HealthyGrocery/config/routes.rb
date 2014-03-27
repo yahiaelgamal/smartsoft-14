a4c1f51c6730f0dcca2a7d0cb3cc0f1239131e73
Mohamed Lotfy <lotfy1893@ubuntu.ubuntu-domain>
2014-03-27 10:34:54 +0200

    story 1.1

--- HealthyGrocery/config/routes.rb
+++ HealthyGrocery/config/routes.rb
@@ -3,6 +3,7 @@ HealthyGrocery::Application.routes.draw do
 
 
   resources :users
+match '/register' =>  'users#new'
 
 
   # The priority is based upon order of creation:
@@ -54,7 +55,7 @@ HealthyGrocery::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'users#new'
 
   # See how all your routes lay out with "rake routes"
 
