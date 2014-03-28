d3c855622ab3d24675edeed2876d58f15b1e09fb
Antoine Foti <tonyfoti10@gmail.com>
2014-03-28 20:20:56 +0200

    Adding my story 2.1

--- HealthyGrocery/app/models/healthrecord.rb
+++ HealthyGrocery/app/models/healthrecord.rb
@@ -1,17 +1,17 @@
 class Healthrecord
   include Mongoid::Document
-# Adding the attributes of the class healthrecords
-  field :name , type: String
+  #Here are the attributes of the class heathrecord
+  field :name, type: String 
   field :bmi, type: Integer
   field :fats, type: Integer
   field :height, type: Integer
   field :weight, type: Integer
   field :program, type: String
   field :bloodtype, type: String
-  
-  # here are the different relationships present
-  belongs_to :user , class_name: 'User' , inverse_of: :records 
+
+#Here declaring the relationships with the class healthrecords
+belongs_to :user , class_name: 'User' , inverse_of: :records 
   has_many :diseases , class_name: 'Disease' , inverse_of: :records
 
- 
+
 end
