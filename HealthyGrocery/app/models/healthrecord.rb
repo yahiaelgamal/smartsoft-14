8c26072d343acc49ac4b5314767f54d3d7952b39
Antoine Foti <tonyfoti10@gmail.com>
2014-03-28 18:52:54 +0200

    Adding my story 2.1

--- HealthyGrocery/app/models/healthrecord.rb
+++ HealthyGrocery/app/models/healthrecord.rb
@@ -1,6 +1,6 @@
 class Healthrecord
   include Mongoid::Document
-
+ # here are the different attributes of class healthrecord
   field :name , type: String
   field :bmi, type: Integer
   field :fats, type: Integer
@@ -9,8 +9,24 @@ class Healthrecord
   field :program, type: String
   field :bloodtype, type: String
   
-  
+   # checks if bmi is present
+    validates :bmi , :presence => true 
+   # checks if name is present 
+    validates :name, :presence => true
+   # checks if fats  is present
+    validates :fats, :presence => true
+   # checks if height is present
+    validates :height, :presence => true
+   # checks if weight is present
+    validates :weight, :presence => true
+   # checks if program is present
+    validates :program, :presence => true
+    # checks if bloodtype is present
+    validates :bloodtype, :presence => true
+   
+  # declare a relationship between users and records
   belongs_to :user , class_name: 'User' , inverse_of: :records 
+  # declare a relationship between diseases and records
   has_many :diseases , class_name: 'Disease' , inverse_of: :records
 
  
