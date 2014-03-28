3b1a6f399ab2d90d423c1ee91cf3a6b6d74179e6
karim-elbawab <karim.elbawab@gmail.com>
2014-03-28 11:29:43 +0200

    Documntation and some changes in the gui

--- HealthyGrocery/app/models/shipment.rb
+++ HealthyGrocery/app/models/shipment.rb
@@ -1,5 +1,11 @@
+# == Schema Information
+#
+# Table name: shipments
+#
+#  id             :integer      not null, primary key
+#  cut_off_time          :integer
 class Shipment
   include Mongoid::Document
   field :cut_off_time, type: Integer
-  has_many :trucks , class_name: 'Truck' ,  :inverse_of => :shipment
+  has_many :trucks
 end
