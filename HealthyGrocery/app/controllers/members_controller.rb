11a0fe9a788b24f2b1d1ad6f6b35b6e78af94afc
mostafa-adel <mostafaadel174@gmail.com>
2014-04-23 21:55:17 +0200

    after change file spaces

--- HealthyGrocery/app/controllers/members_controller.rb
+++ HealthyGrocery/app/controllers/members_controller.rb
@@ -33,12 +33,12 @@ end
 
 
 def edit
-   @member = Member.find(params[:member_id])
+  @member = Member.find(params[:member_id])
    
-   #Author : mostafa adel
-   #team : 3
-   #method: edit profile
-   #params : @member
+  #Author : mostafa adel
+  #team : 3
+  #method: edit profile
+  #params : @member
 end
 
 
