b344cb28c0e8508e505968f834b3f18f11ff4015
ZzimBoo91 <zzimboo91@gmail.com>
2014-04-25 00:07:15 +0200

    Changed the layout of a button and fixed the validations that were commented out, noticed by Jihan

--- HealthyGrocery/app/models/driver.rb
+++ HealthyGrocery/app/models/driver.rb
@@ -5,9 +5,9 @@ class Driver
   field :phone, type: Integer
   field :address, type: String
 
-  #validates :name, presence: true
-  #validates :phone, presence: true, numericality: true
-  #validates :address, presence: true
+  validates :name, presence: true
+  validates :phone, presence: true, numericality: true
+  validates :address, presence: true
   # Author: Hazem Amin
   # Component: 5 
   #Added some validations in order not to get bad data
