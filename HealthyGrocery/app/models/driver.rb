4fe20d6fc337f1dbc6bf15b1da5cbedbf39601e3
ZzimBoo91 <zzimboo91@gmail.com>
2014-04-24 15:35:58 +0200

    Just added some basic documentations

--- HealthyGrocery/app/models/driver.rb
+++ HealthyGrocery/app/models/driver.rb
@@ -5,3 +5,6 @@ class Driver
   field :phone, type: Integer
   field :address, type: String
 end
+  # Author: Hazem Amin
+  # Component: 5
+  # Creating the driver with the attributes defined in story 5.7
