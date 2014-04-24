4bd134f86f15437c50db61dbc2dd5826c0206c63
mostafa-adel <mostafaadel174@gmail.com>
2014-04-24 22:30:23 +0200

    everything is ok

--- HealthyGrocery/config/routes.rb
+++ HealthyGrocery/config/routes.rb
@@ -11,20 +11,20 @@ HealthyGrocery::Application.routes.draw do
   resources :wishlines
 
 
-  resources :wishlists
+   resources :wishlists
 
 
- root :to => 'members#index' # so as to not for the member to root to the page containnng site members!!
+   root :to => 'members#index' # so as to not for the member to root to the page containnng site members!!
 
  devise_for :members, :controllers => {:registrations => "registrations", }
  
- #for user profile page
- resources :members do
-  get 'edit'
+   #for user profile page
+   resources :members do
+   get 'edit'
    
  end
 
-
+  match 'user_root' => redirect("/member/show")
   resources :members
  
 
