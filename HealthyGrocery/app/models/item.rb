7ed05bead5b29da4a720fd5bd646be50e3b2e746
ZzimBoo91 <zzimboo91@gmail.com>
2014-05-13 23:36:07 +0200

    the issue that my scenario reviewer detected was solved

--- HealthyGrocery/app/models/item.rb
+++ HealthyGrocery/app/models/item.rb
@@ -1,87 +1,343 @@
-class Item
-  include Mongoid::Document
-  include Mongoid::Paperclip
+class ItemsController < ApplicationController
+  # (GUI TEAM) This line is made so that the /items does not follow bootstrap
+  layout false
+  # GET /items
+  # GET /items.json
+  # shows all the items in the table item
+  def index
+    if current_member.email == 'admin@gmail.com'
+      @admin = true
+    else
+      @admin = false
+    end
+    @items = Item.all
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @items }
+    end
+  end
 
-  field :price , type: Float
-  field :amount , type: Float
-  field :name , type: String
-  field :description , type: String
-  field :rating , type: Float
-  field :status , type: Boolean
-  field :category , type: String
-  # Author: Hazem Amin
-  # Team: 5
-  # Function: Added new needed attributes for the items-class
-  field :protein , type: Float
-  field :carbohydrate , type: Float
-  field :calcium , type: Float
-  field :fat , type: Float
-  # Author: Hazem
-  # Component: 5
-  # Attribute: paused, to keep track whether each and every item is
-  # paused or not (boolean)
-  field :paused , type: Boolean
-  # Author: Hazem Amin
-  # Team: 5
-  # Function: Added other needed attributes for the items-class
-  field :vitamin_a , type: Float
-  field :vitamin_b , type: Float
-  field :vitamin_c , type: Float
-  field :vitamin_d , type: Float
-  field :vitamin_e , type: Float
-  field :vitamin_k , type: Float
-  # checks if price is a number greater than 0
-  validates :price , :presence => true , :numericality => { :greater_than_or_equal_to => 0 }
-  # checks if name is present
-  validates :name, :presence => true
-  # checks if category  is present
-  validates :category, :presence => true
-  # checks if description is present
-  validates :description, :presence => true
-  # checks if amount is present
-  validates :amount, :presence => true, :numericality => { :greater_than_or_equal_to => 0 }
-  # declares an attached file as an attribute
-  has_mongoid_attached_file :image
-  # checks the attachment type is image
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-  # checks if image is present
-  validates :image, :attachment_presence => true
+  # GET /items/1
+  # GET /items/1.json
+  # show a certain item from a table item using the attribute id
+  def show
+    @item = Item.find(params[:id])
 
-  validates :protein , :presence => true , :numericality => { :greater_than_or_equal_to => 0 }
-  validates :carbohydrate , :presence => true , :numericality => { :greater_than_or_equal_to => 0 }
-  validates :calcium , :presence => true , :numericality => { :greater_than_or_equal_to => 0 }
-  validates :fat , :presence => true , :numericality => { :greater_than_or_equal_to => 0 }
-  # Author: Hazem Amin
-  # Team: 5
-  # Function: Added validations to make sure the submitted float-values of the
-  # nutritions are positive values and that no text-fields are being
-  # submitted with no values (empty)
-  validates :vitamin_a, :presence => true, :numericality => {:greater_than_or_equal_to => 0}
-  validates :vitamin_a, :presence => true, :numericality => {:greater_than_or_equal_to => 0}
-  validates :vitamin_b, :presence => true, :numericality => {:greater_than_or_equal_to => 0}
-  validates :vitamin_c, :presence => true, :numericality => {:greater_than_or_equal_to => 0}
-  validates :vitamin_d, :presence => true, :numericality => {:greater_than_or_equal_to => 0}
-  validates :vitamin_e, :presence => true, :numericality => {:greater_than_or_equal_to => 0}
-  validates :vitamin_k, :presence => true, :numericality => {:greater_than_or_equal_to => 0}
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @item }
+    end
+  end
+
+  # GET /items/new
+  # GET /items/new.json
+  # renders the information from the form then creates a new unsaved record
+  def new
+    @item = Item.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @item }
+    end
+  end
+
+  # GET /items/1/edit
+  # gets the item to be edited and store it in variable @item
+  def edit
+    @item = Item.find(params[:id])
+  end
 
 
-  # declares a relationship between packages and items
-  has_and_belongs_to_many :packages , class_name: "Package" , inverse_of: :items
-  # declares a relationship between orders and items
-  has_and_belongs_to_many :orders , class_name: "Order" , inverse_of: :items
+  def toggle_pause
+    @item = Item.find(params[:id])
+
+    if @item.amount <= 0 && @item.paused == false
+      flash[:notice] = "Can't resume because stock equals #{@item.amount}"
+    else
+      @item.paused = !@item.paused
+      @item.save
+      flash[:notice] = "Item toggled successfully"
+    end
+
+    redirect_to items_url
+  end
+  #Author: Hazem Amin
+  #Method_Name: toggle_pause
+  #What does it do? It simply finds that specific item that needs to be paused and pauses
+  # it if the conditions applies that the item is not out of stock.
+  #Author: Hazem Amin
+  #Method_Name: toggle_pause
+  #What does it do? It simply finds that specific item that needs to be paused and pauses
+  # it if the conditions applies that the item is not out of stock.
+
+  #Author: Hazem Amin
+  #Function: Added the condition that the amount exists, because in case
+  # no amount is entered, the amount will neither be positive nor negative
+  # and thus it would throw an error
+  def create
+    @item = Item.new(params[:item])
+
+    if @item.amount
+      # initial value of paused
+      if @item.amount <= 0
+        @item.paused = false
+      else
+        @item.paused = true
+      end
+    end
+    respond_to do |format|
+      if @item.save
+        format.html { redirect_to @item, notice: 'Item was successfully created.' }
+        format.json { render json: @item, status: :created, location: @item }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  #Author: Hazem Amin
+  #Component: 5
+  #Method_Name: create
+  #What does it do? It simply CEATES an item, as being one of the 4 fundumentals of CRUD.
+  #What about the part commented below? I decide the initial values of paused when it is
+  # created, whether it should be initially paused or resumed.
+  #    if @item.amount <= 0
+  #    @item.paused = true
+  #  else
+  #    @item.paused = false
+  #  end
+
+  #views the users without the create button
+  def viewusers
+    @users = User.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @users }
+    end
+  end
 
 
-  #Author: Jihan Adel
-  #Team: 5
-  #linking items to diseases
+  # PUT /items/1
+  # PUT /items/1.json
+  # updated an existing record taking its info from the def edit
+  def update
+    @item = Item.find(params[:id])
 
-  belongs_to :good_for_diseases , class_name: 'Disease' , inverse_of: :recommended_items
-  belongs_to :bad_for_diseases , class_name: 'Disease' , inverse_of: :restricted_items
+    respond_to do |format|
+      if @item.update_attributes(params[:item])
+        format.html { redirect_to @item, notice: 'Item was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
+  # DELETE /items/1
+  # DELETE /items/1.json
+  # deletes a record
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    respond_to do |format|
+      format.html { redirect_to items_url }
+      format.json { head :no_content }
+    end
+  end
 
-  #Author: Abdelrahman Sakr
-  #Team : 1
-  #Declaring a relationship between the two classes Item, and Lineitem.
-  has_many :lineitems, class_name: "Lineitem"
+  #Author: Sameh Metias
+  #team: 3
+  #function: retreives all teh items in the instance variable @items in order to show the items from
+  #the member point of view
+
+  #AUTHOR: Mahmoud Eldesouky
+  #Team :5
+  #parameter : flag, message
+  #sends the message that is set by def Add to the members_item_index view
+  def members_items_index
+    gon.flag =  flash[:flag]
+    gon.message =  flash[:message]
+    #Author: Antoine Foti
+    #Team: 2
+    #Function: After adding the filter search, it retreives all the required items in the instance variable @items after
+    #filtering them according to the different fields the user would like to filter on in order to show the desired items
+    #from the member point of view.
+    #And if the user leaves any field empty without specifying what to filter, it will not be considered as a value and
+    #the filter search will be calculated according to the remaining non-empty filtering fields.
+    if (params[:status] == 'available')
+      @items = Item.where :name => params[:name], :category => params[:category], :price => params[:price], :rating => params[:rating], :status => true
+      if (params[:name] == '')
+        @items = Item.where :category => params[:category], :price => params[:price], :rating => params[:rating], :status => true
+      end
+      if (params[:category] == '')
+        @items = Item.where :name => params[:name], :price => params[:price], :rating => params[:rating], :status => true
+      end
+      if (params[:price] == '')
+        @items = Item.where :category => params[:category], :name => params[:name], :rating => params[:rating], :status => true
+      end
+      if (params[:rating] == '')
+        @items = Item.where :category => params[:category], :name => params[:name], :price => params[:price], :status => true
+      end
+      if (params[:name] == '') && (params[:category] == '')
+        @items = Item.where :rating => params[:rating], :price => params[:price], :status => true
+      end
+      if (params[:name] == '') && (params[:price] == '')
+        @items = Item.where :rating => params[:rating], :category => params[:category], :status => true
+      end
+      if (params[:name] == '') && (params[:rating] == '')
+        @items = Item.where :category => params[:category], :price => params[:price], :status => true
+      end
+      if (params[:category] == '') && (params[:price] == '')
+        @items = Item.where :name => params[:name], :rating => params[:rating], :status => true
+      end
+      if (params[:category] == '') && (params[:rating] == '')
+        @items = Item.where :name => params[:rating], :price => params[:price], :status => true
+      end
+      if (params[:price] == '') && (params[:rating] == '')
+        @items = Item.where :name => params[:name], :category => params[:category], :status => true
+      end
+      if (params[:name] == '') && (params[:category] == '') && (params[:price] == '')
+        @items = Item.where :rating => params[:rating], :status => true
+      end
+      if (params[:name] == '') && (params[:category] == '') && (params[:rating] == '')
+        @items = Item.where :price => params[:price], :status => true
+      end
+      if (params[:name] == '') && (params[:rating] == '') && (params[:price] == '')
+        @items = Item.where :category => params[:category], :status => true
+      end
+      if (params[:rating] == '') && (params[:category] == '') && (params[:price] == '')
+        @items = Item.where :name => params[:name], :status => true
+      end
+      if (params[:name] == '') && (params[:category] == '') && (params[:price] == '') && (params[:rating] == '')
+        @items = Item.where :status => true
+      end
+    else
+      @items = Item.where :name => params[:name], :category => params[:category], :price => params[:price], :rating => params[:rating]
+      if (params[:name] == '')
+        @items = Item.where :category => params[:category], :price => params[:price], :rating => params[:rating]
+      end
+      if (params[:category] == '')
+        @items = Item.where :name => params[:name], :price => params[:price], :rating => params[:rating]
+      end
+      if (params[:price] == '')
+        @items = Item.where :category => params[:category], :name => params[:name], :rating => params[:rating]
+      end
+      if (params[:rating] == '')
+        @items = Item.where :category => params[:category], :name => params[:name], :price => params[:price]
+      end
+      if (params[:name] == '') && (params[:category] == '')
+        @items = Item.where :rating => params[:rating], :price => params[:price]
+      end
+      if (params[:name] == '') && (params[:price] == '')
+        @items = Item.where :rating => params[:rating], :category => params[:category]
+      end
+      if (params[:name] == '') && (params[:rating] == '')
+        @items = Item.where :category => params[:category], :price => params[:price]
+      end
+      if (params[:category] == '') && (params[:price] == '')
+        @items = Item.where :name => params[:name], :rating => params[:rating]
+      end
+      if (params[:category] == '') && (params[:rating] == '')
+        @items = Item.where :name => params[:rating], :price => params[:price]
+      end
+      if (params[:price] == '') && (params[:rating] == '')
+        @items = Item.where :name => params[:name], :category => params[:category]
+      end
+      if (params[:name] == '') && (params[:category] == '') && (params[:price] == '')
+        @items = Item.where :rating => params[:rating]
+      end
+      if (params[:name] == '') && (params[:category] == '') && (params[:rating] == '')
+        @items = Item.where :price => params[:price]
+      end
+      if (params[:name] == '') && (params[:rating] == '') && (params[:price] == '')
+        @items = Item.where :category => params[:category]
+      end
+      if (params[:rating] == '') && (params[:category] == '') && (params[:price] == '')
+        @items = Item.where :name => params[:name]
+      end
+      if (params[:name] == '' || params[:name].nil?) && (params[:category] == '' || params[:category].nil?) && (params[:price] == '' || params[:price].nil?) && (params[:rating] == '' || params[:rating].nil?)
+        @items = Item.all
+      end
+    end
+  end
+
+  #AUTHOR: Mahmoud Eldesouky
+  #Team :5
+  #parameter : item, amount
+  #checks that the choosen item with its nutrition does not exceed the acceptable limit of
+  #this user nutrients that we keep track of through his healthrecord. Each time an item within the
+  #nutrition limits is choosen his till_now attribute in the healthrecord is updated else if a violation happens
+  #the message attribute is set with the approprite message to the user describing his violations
+  def add
+
+    item_id = params[:item]
+    amount = params[:amount].to_i
+    item =  Item.where(id:  item_id).first
+
+    if item.amount > 0
+      user = current_member
+      healthrecord =  user.records.first
+
+      if  healthrecord.blank?
+        @message = "Please fill your health record before ordering"
+        @flag = false
+        flash[:message] = @message
+        flash[:flag] = @flag
+        redirect_to :action => :members_items_index
+        return
+      end
+
+      @flag = true
+      @message = "This request with its quantity exceeds the acceptable range of the following: "
+      v_counter=1
+
+      if  healthrecord.acceptable_protein_per_week < ( (item.protein * amount )+  healthrecord.protein_till_now)
+        @message = @message+v_counter.to_s+".proteins "
+        v_counter = v_counter+1
+        @flag = false
+      end
+      if  healthrecord.acceptable_carbohydrate_per_week < ( (item.carbohydrate * amount) +  healthrecord.carbohydrate_till_now)
+        @message = @message + v_counter.to_s+".carbohydrates "
+        v_counter = v_counter+1
+        @flag = false
+      end
+      if  healthrecord.acceptable_calcium_per_week < ( (item.calcium * amount)+  healthrecord.calcium_till_now)
+        @message = @message +v_counter.to_s+ ".calcium "
+        v_counter = v_counter+1
+        @flag = false
+      end
+      if healthrecord.acceptable_fat_per_week < ( (item.fat * amount) +  healthrecord.fat_till_now)
+        @message = @message +v_counter.to_s+ ".fats"
+        v_counter = v_counter+1
+        @flag = false
+      end
+      if (amount === 0 )
+        @flag = false
+        @message = "please enter the quantity"
+      end
+
+      if  @flag
+
+        protein =  healthrecord.protein_till_now +  (item.protein * amount )
+        carbohydrate =  healthrecord.carbohydrate_till_now +  (item.carbohydrate * amount)
+        calcium  =   healthrecord.calcium_till_now +  (item.calcium * amount)
+        fat  =   healthrecord.fat_till_now +  (item.fat * amount)
+
+        Member.where(id: user.id).first.records.update(protein_till_now: protein)
+        Member.where(id: user.id).first.records.update(carbohydrate_till_now: carbohydrate)
+        Member.where(id: user.id).first.records.update(calcium_till_now: calcium)
+        Member.where(id: user.id).first.records.update(fat_till_now: fat)
+
+      end
+    else
+      @message = "Sorry! This item is not available in stock"
+
+    end
+    flash[:message] = @message
+    flash[:flag] = @flag
+    redirect_to :action => :members_items_index
+  end
 
 end
