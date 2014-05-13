63d1c4be3d04e92bbb8f1db2eb8022a3914ea567
MennatallahElKharbotly <menna.elkharbotly@gmail.com>
2014-05-13 15:03:07 -0700

    #277 as a retailer,I can set a specific time for the truck trip

--- HealthyGrocery/app/models/truck.rb
+++ HealthyGrocery/app/models/truck.rb
@@ -3,6 +3,13 @@ class Truck
   field :driver, type: String
   field :capacity, type: Integer
   field :status, type: Boolean
+
+#Author: Mennatallah Adel
+#Team : 4
+#Model : Truck
+#This is a field of type Integer...its the triptime for the truck and it's added to every view of the truck
+
+field :tripTime, type: Integer
 #Author : Ahmed AbdElsattar
 #Team: 4
 #attr :      has_many :routes , class_name: 'Route'  , :inverse_of => :truck
