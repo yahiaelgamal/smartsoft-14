c992664e9417edb1b423fa52663c6fbb28a03363
andrewkhouzam <andrew.khouzam@gmail.com>
2014-05-13 19:41:44 +0200

    Issue #275 redirecting to verify page after editing the truck

--- HealthyGrocery/app/controllers/trucks_controller.rb
+++ HealthyGrocery/app/controllers/trucks_controller.rb
@@ -41,6 +41,10 @@ class TrucksController < ApplicationController
     
   end
 
+  def verify
+        @truck = Truck.find(params[:id])
+  end
+
   # POST /trucks
   # POST /trucks.json
   def create
@@ -64,7 +68,7 @@ class TrucksController < ApplicationController
 
     respond_to do |format|
       if @truck.update_attributes(params[:truck])
-        format.html { redirect_to @truck, notice: 'Truck was successfully updated.' }
+        format.html { redirect_to verifyOrder_path(@truck), notice: 'Please Verify the Orders' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
