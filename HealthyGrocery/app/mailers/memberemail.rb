da5e0ad1ce2fee1c4779afec5110c012e0905339
lotfy1893 <mohamed.lotfy.hammad@gmail.com>
2014-05-14 12:21:54 +0300

    Delete membermail.rb

--- HealthyGrocery/app/mailers/memberemail.rb
+++
@@ -1,9 +0,0 @@
-class Memberemail < ActionMailer::Base
- 	default :from => "admin@healthygrocery.com"
- 	def welcome_email(member,token)
-    @member = member
-	@token = token
-    @url  = 'http://localhost:3000/members/sign_in'
-    mail(to: member.email, subject: 'Welcome to Our Awesome Site')
-  end
-end
