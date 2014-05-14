dea502b2d01c431dc8f01c292c2aea63a98303b4
andrewkhouzam <andrew.khouzam@gmail.com>
2014-05-14 22:31:32 +0200

    Updating the code view in device_helper.rb

--- HealthyGrocery/app/helpers/devise_helper.rb
+++ HealthyGrocery/app/helpers/devise_helper.rb
@@ -22,8 +22,8 @@ def devise_error_messages!
     errors = resource.errors.empty? ? flash_alerts : resource.errors.full_messages
 
     messages = errors.map { |msg| content_tag(:li, msg) }.join
-    sentence = I18n.t(error_key, :count    => errors.count,
-                                 :resource => resource.class.model_name.human.downcase)
+    sentence = I18n.t(error_key, count: errors.count,
+                                 resource: resource.class.model_name.human.downcase)
 
     html = <<-HTML
     <div id="error_explanation">
