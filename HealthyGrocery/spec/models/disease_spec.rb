2496b0b4a5d293dec9344be782c49f7abc9b8ee5
jihan2032 <jihan2032@gmail.com>
2014-05-14 23:38:40 +0200

    #167 #168 start making the test

---
+++ HealthyGrocery/spec/models/disease_spec.rb
@@ -0,0 +1,3 @@
+# spec/models/contact.rb require 'spec_helper' describe Contact do it "has a valid factory" it "is invalid without a firstname" it "is invalid without a lastname" it "returns a contact's full name as a string" end - See more at: http://everydayrails.com/2012/03/19/testing-series-rspec-models-factory-girl.html#sthash.W1wKWlqx.dpuf
+
+describe Contact do it "has a valid factory" it "is invalid without a firstname" it "is invalid without a lastname" it "returns a contact's full name as a string - See more at: http://everydayrails.com/2012/03/19/testing-series-rspec-models-factory-girl.html#sthash.W1wKWlqx.dpuf
