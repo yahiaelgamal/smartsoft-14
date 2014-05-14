1541dbf1f0b15addce20123f3b0a79bdfc630706
karim-elbawab <karim.elbawab@gmail.com>
2014-05-15 00:05:53 +0200

    Issue #270 removing some  unused files

--- HealthyGrocery/app/controllers/shipments_controller.rb
+++ HealthyGrocery/app/controllers/shipments_controller.rb
@@ -20,37 +20,4 @@ class ShipmentsController < ApplicationController
       format.json { render json: @shipment }
     end
   end
-
-  # GET /shipments/1/edit
-  def edit
-    @shipment = Shipment.find(params[:id])
-  end
-
-  # PUT /shipments/1
-  # PUT /shipments/1.json
-  def update
-    @shipment = Shipment.find(params[:id])
-
-    respond_to do |format|
-      if @shipment.update_attributes(params[:shipment])
-        format.html { redirect_to @shipment, notice: 'Shipment was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @shipment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /shipments/1
-  # DELETE /shipments/1.json
-  def destroy
-    @shipment = Shipment.find(params[:id])
-    @shipment.destroy
-
-    respond_to do |format|
-      format.html { redirect_to shipments_url }
-      format.json { head :no_content }
-    end
-  end
 end
