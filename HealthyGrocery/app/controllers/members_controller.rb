5bc70fd3af9edfcca316f5b63ce3527cfd12b4cb
mostafa-adel <mostafaadel174@gmail.com>
2014-05-15 18:02:34 +0200

    #262_ after handling the search insenstive cases

--- HealthyGrocery/app/controllers/members_controller.rb
+++ HealthyGrocery/app/controllers/members_controller.rb
@@ -1,120 +1,131 @@
 class MembersController < ApplicationController
   before_filter :authenticate_member!
-   
-#Author : mohamed lotfy
-#team : 1
-#method:index
-#params : none
-#it lists all the users on the database it has a boolean check for a variable called admin to controll visibilty of all the members for only the admin 
-def index
 
-    @members = Member.all.page(params[:page]).per(5)
+  #Author : mohamed lotfy
+  #team : 1
+  #method:index
+  #params : none
 
-    if (!params[:search].nil?) 
-      @members= Member.where(:first_name => params[:search]).page(params[:page]).per(5)
-      end
-  if current_member.email == 'admin@gmail.com'
+  #it lists all the users on the database it has a boolean check for a variable called admin to controll   visibilty of all the members for only the admin
+
+
+  # Author : Mostafa Adel
+  # Team   : 3
+  # Method : Index
+  # Description : Retailer can see all members  with  pagination and search for them.
+  def index
+
+    @members = Member.all.page(params[:page]).per(20)
+
+    if (!params[:search].nil?)
+       @members= Member.where(:first_name => /^#{params[:search]}/i).page(params[:page]).per(20)
+
+    end
+
+    if current_member.email == 'admin@gmail.com'
       @admin = true
-  else 
+    else
       @admin = false
-  end    
-end
-#Author : mohamed lotfy
-#team : 1
-#method: show
-#params : none
-#it shows the page of the user signed in or signed up it has the same check to differentiate between the admin and the user page
-#def show
- #   @member = Member.find(params[:id])
+    end
+  end
+  #Author : mohamed lotfy
+  #team : 1
+  #method: show
+  #params : none
+  #it shows the page of the user signed in or signed up it has the same check to differentiate between the admin and the user page
+  #def show
+  #   @member = Member.find(params[:id])
   #if current_member.email == 'admin@gmail.com'
   #end
 
-     
+
 
   def show
-@member = Member.find(params[:id])
+    @member = Member.find(params[:id])
 
 
 
 
-  	if current_member.email == 'admin@gmail.com'
+    if current_member.email == 'admin@gmail.com'
 
-  		@admin = true
-  else 
-	    @admin = false
-  end
-   
+      @admin = true
+    else
+      @admin = false
+    end
 
-  if current_member.wishlist.nil?
+
+
+    if current_member.wishlist.nil?
       @wishexist = false
-  else 
+    else
       @wishexist = true
+    end
   end
-end
 
-   #(GUI Team) This session is used to be able to diffirentiate from normal User/Admin 
-#session["isAdmin"]= @admin
-#author: Fatma EmranS
-#Team 2
-#Funtion: Checkboxes for the people with health records of the user who wants to order.
-def order_checkboxes
-  @healthrecords = current_member.records
-  @records_ids = params[:all]
-  current_member.active_records = @records
-  current_member.save
-end
+  #(GUI Team) This session is used to be able to diffirentiate from normal User/Admin
+  #session["isAdmin"]= @admin
+  #author: Fatma EmranS
+  #Team 2
+  #Funtion: Checkboxes for the people with health records of the user who wants to order.
+  def order_checkboxes
+    @healthrecords = current_member.records
+    @records_ids = params[:all]
+    current_member.active_records = @records
+    current_member.save
+  end
 
-  
-#Author : mina sedra
-#team : 2
-#method: get healthrecord
-#params : none
-#it shows the page of the member's healthrecord 
- def get_records
+
+  #Author : mina sedra
+  #team : 2
+  #method: get healthrecord
+  #params : none
+  #it shows the page of the member's healthrecord
+  def get_records
+
     @member = Member.find(params[:id])
     @healthrecord = @member.records
-  respond_to do |format|
+    respond_to do |format|
       format.html# show.html.erb
       format.json { render json: @healthrecord }
-  end
+    end
   end
 
- 
+
   def show_restricted_items
-    
+
     @member = current_member
-    
+
     if current_member.email == 'admin@gmail.com'
-        @admin = true
-    else 
+      @admin = true
+    else
       @admin = false
     end
   end
 
   # def show_ideal_calories
-    
+
   #   @member = current_member
-    
+
   #   if current_member.email == 'admin@gmail.com'
   #       @admin = true
-  #   else 
+  #   else
   #     @admin = false
   #   end
   # end
 
   def calculated_ideal
-    
+
     @member = current_member
-    
+
     if current_member.email == 'admin@gmail.com'
-        @admin = true
-    else 
+      @admin = true
+    else
       @admin = false
     end
   end
 
 
-  
+
   #Atuhor: ahmed helali
   #team 2
   #method: show_ideal_calories
@@ -122,20 +133,25 @@ end
   #function: reponsible for the view of the same name
 
   def show_ideal_calories
-    
+
     @member = current_member
     @weight = ""
     @duration = ""
-    
+
     if current_member.email == 'admin@gmail.com'
-        @admin = true
-    else 
+      @admin = true
+    else
       @admin = false
     end
   end
 
-
-end  
-
-
-
+  #Atuhor: ahmed abdelsattar
+  #team 2
+  #method: generate_routes
+  #parameters: none
+  #function: calling the generate_routes method from Shipment model
+  def generate_routes
+    Shipment.generate_routes
+    redirect_to "/members/hub"
+  end
+end
