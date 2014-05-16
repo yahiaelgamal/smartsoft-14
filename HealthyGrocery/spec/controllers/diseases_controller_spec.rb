3f3032eec90a3a525847c3e573b3b58427d75dce
jihan2032 <jihan2032@gmail.com>
2014-05-16 19:36:26 +0200

    Issues #167 #168 modifiying the controller test

--- HealthyGrocery/spec/controllers/diseases_controller_spec.rb
+++ HealthyGrocery/spec/controllers/diseases_controller_spec.rb
@@ -5,6 +5,9 @@ describe DiseasesController do
 	before :each do
 	Disease.all.destroy	
 	end	
+	def mock_disease(stubs={})
+	  @mock_disease ||= mock_model(Disease, stubs).as_null_object	
+	end
 	describe "GET #index" do
       it "populates an array of diseases" do
         disease = FactoryGirl.create(:disease)
@@ -20,7 +23,7 @@ describe DiseasesController do
       it "assigns the requested disease to @disease" do
         disease = FactoryGirl.create(:disease)
         get :show, id: disease
-        assigns(:disease).should eq([disease]) 
+        assigns(:disease).should eq(disease) 
       end	
       it "renders the #show view" do
         get :show, id: FactoryGirl.create(:disease)
@@ -29,19 +32,20 @@ describe DiseasesController do
     end	
     describe "GET #new" do
       it "assigns a name and information to the new disease" do
+      	disease = FactoryGirl.create(:disease)
         get :new
-        assigns(:disease).name.should eq(["cancer"])
-        assigns(:disease).information.should eq(["common disease"])
-        assigns(:disease).protein.should eq([20])
-        assigns(:disease).carbohydrate.should eq([20])
-        assigns(:disease).calcium.should eq([20])
-        assigns(:disease).fat.should eq([20])
-        assigns(:disease).vitamin_a.should eq([20]) 
-        assigns(:disease).vitamin_b.should eq([20])
-        assigns(:disease).vitamin_c.should eq([20])
-        assigns(:disease).vitamin_d.should eq([20])
-        assigns(:disease).vitamin_e.should eq([20])
-        assigns(:disease).vitamin_k.should eq([20]) 
+        assigns(:disease).name.should eq("cancer")
+        assigns(:disease).information.should eq("common disease")
+        assigns(:disease).protein.should eq(20)
+        assigns(:disease).carbohydrate.should eq(20)
+        assigns(:disease).calcium.should eq(20)
+        assigns(:disease).fat.should eq(20)
+        assigns(:disease).vitamin_a.should eq(20) 
+        assigns(:disease).vitamin_b.should eq(20)
+        assigns(:disease).vitamin_c.should eq(20)
+        assigns(:disease).vitamin_d.should eq(20)
+        assigns(:disease).vitamin_e.should eq(20)
+        assigns(:disease).vitamin_k.should eq(20) 
       end
       it "renders the :new template" do
         get :new
@@ -51,25 +55,37 @@ describe DiseasesController do
     describe "POST #create" do
       context "with valid attributes" do	 
         it "saves the new disease in the database" do
-          expect {
-              post :create, disease: Factory.attributes_for(:disease)
-            }.to change(Disease,:count).by(1)
+         post :create, :disease => {'name' => 'cancery', 'information'=>'ayklam'}
+         assigns(:disease).should be_valid
         end	
         it "redirects to the :index view" do
-          post :create, disease: FactoryGirl.attributes_for(:disease, provider_id: 1)	
-          response.should redirect_to Disease.last
+          post :create, :disease => {'name' => 'cancery', 'information'=>'ayklam'}
+          response.should redirect_to(disease_path(assigns[:disease]))
         end	
       end
       context "with invalid attributes" do	 
         it "does not save the new disease in the database" do
-          expect {
-              post :create, disease: Factory.attributes_for(:invalid_disease)
-            }.to_not change(Disease,:count)
+          post :create, :disease => {'information'=>'ayklam'}
+          flash[:notice].should == 'Name is required.'
+          assigns(:disease).should_not be_valid
         end	
         it "re-renders the :new template" do
-          post :create, disease: FactoryGirl.attributes_for(:invalid_disease, provider_id: 1)
-          response.should render_template :new 
+          response.should render_template :new
         end	
       end
     end	
+    describe 'DELETE destroy' do
+    	before :each do
+    		@disease = FactoryGirl.create(:disease)
+    	end
+    	it "deletes the disease" do
+    	  expect{	
+            delete :destroy, id: @disease
+    	  }.to change(Disease, :count).by(-1)
+    	end	
+    	it "redirects to diseases#index" do 
+    		delete :destroy, id: @diseases
+    		response.should redirect_to (diseases_path)
+    	end
+    end		
 end
