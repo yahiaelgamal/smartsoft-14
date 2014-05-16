4fcab2042111589624640092b98b49f3c6a4d12c
osherifo <osherifo93@gmail.com>
2014-05-16 20:37:37 +0200

    Issue #254 Adding tests to the order_spec.rb

--- HealthyGrocery/spec/models/order_spec.rb
+++ HealthyGrocery/spec/models/order_spec.rb
@@ -2,6 +2,43 @@
 require 'spec_helper'
 
 describe Order do
+
+  #In Order to check Omar Sherif's test type rspec spec/models/order_spec.rb
+
+
+  #Author: Omar Sherif
+  #Team: 4
+  #Function: It calls the function in order.rb called "get optimized orders" based on global address
+  #cheks it on global addresses.
+  
+  it "has an optimized order based on the order's global address" do 
+o1 = Order.create
+o1.address = "New York"
+o2 = Order.create
+o2.address = "Cairo"
+o3 = Order.create
+o3.address = "Melbourne"
+Order.get_optimized_orders([o2,o3,o1],99999999999999999999999999999999999999).should == [o2.address,o3.address,o1.address,0]
+end
+
+
+  #Author: Omar Sherif
+  #Team: 4
+  #Function: checks the optimized order on local addresses
+it "has an optimized order based on the order's local address " do
+
+o1 = Order.create
+o1.address = "Meret Basha, Cairo Governorate, Egypt"
+
+o2 = Order.create
+o2.address = "Lazoughly Square"
+
+o3 = Order.create
+o3.address = "El-Shaikh Rihan"
+
+Order.get_optimized_orders([o2,o3,o1],5000000).should == [o2.address,o3.address,o1.address,9]
+end
+
   it "has a valid factory"  do
     FactoryGirl.create(:order).should be_valid
   end
