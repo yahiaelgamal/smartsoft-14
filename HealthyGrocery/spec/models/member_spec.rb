841963c1dc4c78e4caf3486bc4438745aabaf597
mshebokshy <magdelshebokshy@gmail.com>
2014-05-16 22:56:07 +0200

    adding test

--- HealthyGrocery/spec/models/member_spec.rb
+++ HealthyGrocery/spec/models/member_spec.rb
@@ -2,16 +2,30 @@
 require 'spec_helper'
 
 describe Member do 
-  @disease = FactoryGirl.create(:disease)
-  @item = FactoryGirl.create(:item)
-  @item2 = FactoryGirl.create(:item)
-  @item3 = FactoryGirl.create(:item)
-  @restricted_array = []
-  @restricted_array << @item.id
-  @restricted_array << @item2.id
-  @allowed = []
-  @allowed << @item2.id
-  @allowed << @item2.id
-  @allowed << @item3.id
-
+  describe "testing allowed_items method" do
+    before :each do
+     Member.all.destroy
+     Item.all.destroy
+     Disease.all.destroy
+     @member = FactoryGirl.create(:member)
+     @disease = FactoryGirl.create(:disease)
+     @item = FactoryGirl.create(:item)
+     @item2 = FactoryGirl.create(:item ,name: "Orange")
+     @item3 = FactoryGirl.create(:item, name: "Bate5")
+     @restricted_array = []
+     @restricted_array << @item.id
+     @restricted_array << @item2.id
+     @allowed = []
+     @allowed = Item.all
+     @disease.restricted_items = @restricted_array 
+     @meber.diseases << @disease
+     Member.allowed_items(@member, @allowed)
+    end
+    it "test number of allowed_items " do
+      Member.last.allowed_items.count.should == 1 
+    end
+    it "test name of allowed_items " do
+      Member.last.allowed_items[0].name.should == "Bate5" 
+    end
+  end
 end
